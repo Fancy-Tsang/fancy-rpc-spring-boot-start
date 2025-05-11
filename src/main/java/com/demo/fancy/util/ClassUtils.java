a1ecaa3502353fa9ef250628d7cb275652f99e20
曾繁曦 <2628690042@qq.com>
2025-05-11 12:32:42 +0800

    util

---
+++ src/main/java/com/demo/fancy/util/ClassUtils.java
@@ -0,0 +1,26 @@
+package com.demo.fancy.util;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * @author fancy
+ * @version 1.0.0
+ * @description:
+ * @date 2025/03/18 15:44
+ */
+public class ClassUtils {
+
+    public static final String CLASS_EXTENSION = ".class";
+
+    public static final String JAVA_EXTENSION = ".java";
+
+    public static URI toURI(String name) {
+        try {
+            return new URI(name);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/demo/fancy/util/ReflectUtils.java
@@ -0,0 +1,40 @@
+package com.demo.fancy.util;
+
+import java.lang.reflect.Method;
+
+/**
+ * @Author: fancy
+ * @Description:
+ * @Date: Created in 2025/3/15
+ */
+public class ReflectUtils {
+
+    /**
+     * @param method
+     * @return
+     */
+    public static String[] getParameterTypeNames(Method method) {
+        Class<?>[] parameterTypes = method.getParameterTypes();
+        String[] arr = new String[parameterTypes.length];
+        for (int i = 0; i < parameterTypes.length; i++) {
+            arr[i] = parameterTypes[i].getName();
+        }
+        return arr;
+    }
+
+    /**
+     * @param parameterTypeNames
+     * @return
+     */
+    public static Class[] convertToParameterTypes(String[] parameterTypeNames) {
+        Class[] arr = new Class[parameterTypeNames.length];
+        for (int i = 0; i < parameterTypeNames.length; i++) {
+            try {
+                arr[i] = Class.forName(parameterTypeNames[i]);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        return arr;
+    }
+}
